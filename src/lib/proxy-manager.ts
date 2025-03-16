93750fb9c2739a91d17700591216acf521db1fa7
vacuumDev <vacuum_gh@proton.me>
2025-03-16 13:23:42 +0200

    reset index check for error

--- src/lib/proxy-manager.ts
+++ src/lib/proxy-manager.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import RedisWorker from "./redis-worker.js";
+import {HttpsProxyAgent} from "https-proxy-agent";
 
 const proxies = fs.readFileSync('data/proxies.txt').toString().split('\n');
 const config = JSON.parse(fs.readFileSync("data/config.json", "utf8"));
@@ -15,6 +16,15 @@ export default class ProxyManager {
             await RedisWorker.resetIndex();
 
         index = await RedisWorker.getNext();
-        return proxies[index] as string;
+        let proxy = proxies[index] as string;
+        try {
+            const agent = new HttpsProxyAgent(proxy);
+        } catch (e) {
+            await RedisWorker.resetIndex();
+            index = await RedisWorker.getNext();
+            proxy = proxies[index] as string;
+        }
+
+        return proxy;
     }
 }
