7bb2fd576ecd8380f8cc3e5b28f6e81334f52aa2
vacuumDev <vacuum_gh@proton.me>
2025-03-24 20:10:01 +0200

    two versions, old and new support

--- src/accounts-migration.ts
+++ src/accounts-migration.ts
@@ -14,38 +14,68 @@ const main = async () => {
         const fileContent = fs.readFileSync(readyAccountsPath, 'utf-8');
         const lines = fileContent.split('\n').filter(line => line.trim() !== '');
         for (const line of lines) {
-            const parts = line.split('|');
-            if (parts.length >= 6) {
-                const email = parts[0];
+            if(line.includes('|')) {
+                const parts = line.split('|');
+                if (parts.length >= 6) {
+                    const email = parts[0];
 
-                if (accounts.some((acc: any) => acc.login === email)) {
-                    logger.debug(`Account ${email} already exists in config, skipping.`);
-                    continue;
-                }
+                    if (accounts.some((acc: any) => acc.login === email)) {
+                        logger.debug(`Account ${email} already exists in config, skipping.`);
+                        continue;
+                    }
 
-                const password = parts[4];
-                const stickyProxy = parts[5];
-                const accessToken = parts[6];
-                const userId = parts[7];
-                const userAgent = parts[8];
-                try {
-                    await RedisWorker.setSession(email as string, JSON.stringify({
-                        accessToken: accessToken,
-                        userId: userId
-                    }));
-                    accounts.push({
-                        login: email,
-                        proxyThreads: 200,
-                        userAgent,
-                        stickyProxy,
-                        password
-                    });
-                    logger.debug(`Redis session set for ${email}`);
-                } catch (err) {
-                    logger.debug(`Failed to set session for ${email}: ${err}`);
+                    const password = parts[4];
+                    const stickyProxy = parts[5];
+                    const accessToken = parts[6];
+                    const userId = parts[7];
+                    const userAgent = parts[8];
+                    try {
+                        await RedisWorker.setSession(email as string, JSON.stringify({
+                            accessToken: accessToken,
+                            userId: userId
+                        }));
+                        accounts.push({
+                            login: email,
+                            proxyThreads: 200,
+                            userAgent,
+                            stickyProxy,
+                            password
+                        });
+                        logger.debug(`Redis session set for ${email}`);
+                    } catch (err) {
+                        logger.debug(`Failed to set session for ${email}: ${err}`);
+                    }
+                } else {
+                    logger.debug(`Skipping invalid line in ready_accounts.txt: ${line}`);
                 }
             } else {
-                logger.debug(`Skipping invalid line in ready_accounts.txt: ${line}`);
+                const parts = line.split(':');
+                if (parts.length >= 6) {
+                    const email = parts[0];
+
+                    if (accounts.some((acc: any) => acc.login === email)) {
+                        logger.debug(`Account ${email} already exists in config, skipping.`);
+                        continue;
+                    }
+
+                    const accessToken = parts[4];
+                    const userId = parts[5];
+                    try {
+                        await RedisWorker.setSession(email as string, JSON.stringify({
+                            accessToken: accessToken,
+                            userId: userId
+                        }));
+                        accounts.push({
+                            login: email,
+                            proxyThreads: 200
+                        });
+                        logger.debug(`Redis session set for ${email}`);
+                    } catch (err) {
+                        logger.debug(`Failed to set session for ${email}: ${err}`);
+                    }
+                } else {
+                    logger.debug(`Skipping invalid line in ready_accounts.txt: ${line}`);
+                }
             }
         }
     } else {
