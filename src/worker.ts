da0b5fe2cebaf06dc60af3163066a8fd2badd583
vacuumDev <vacuum_gh@proton.me>
2025-03-25 23:18:33 +0200

    fix delays

--- src/worker.ts
+++ src/worker.ts
@@ -1,19 +1,23 @@
 import Grass from "./lib/grass.js";
 import RedisWorker from "./lib/redis-worker.js";
+import fs from "fs";
 
 const delay = async (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
+
+const config = JSON.parse(fs.readFileSync('data/config.json', 'utf-8'));
+
 const processGrassAccount = async (login: string, password: string, stickyProxy: string, rotatingProxy: string, proxyThreads: number, isPrimary: boolean, userAgent: string) => {
     await RedisWorker.init();
     const promises = [];
 
-    const min = 1000, max = 10_000;
-    const ms = Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = config.delay[0], max = config.delay[1];
 
     for (let i = 0; i < proxyThreads; i++) {
         const isLowAmount = isPrimary && proxyThreads < 30;
+        const ms = Math.floor(Math.random() * (max - min + 1)) + min;
         await delay(ms)
         const grass = new Grass(i, isPrimary && i === 0, userAgent, isLowAmount);
         promises.push(grass.startMining(login, password, stickyProxy, rotatingProxy));
